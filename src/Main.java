9d84cf0e006e38b1144154796ffaf3b667b77612
zakariabanid101 <banidzakaria2@gmail.com>
2025-03-14 14:24:22 +0000

    creare new employees

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        Employee employee1 = new Employee("adam",1000,"it",1);
+        Employee employee2 = new Employee("abraham",1200,"it",2);
+        Employee employee3 = new Employee("noah",1130,"it",3);
+
+
     }
 }
