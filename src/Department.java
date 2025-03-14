4f017d24274cea0e451da41ac72df0118ca35b8f
zakariabanid101 <banidzakaria2@gmail.com>
2025-03-14 14:49:50 +0000

    add a function on department class to add employees

--- src/Department.java
+++ src/Department.java
@@ -1,5 +1,15 @@
 public class Department {
     private String name;
-    private Employee[] employees;
+    private final Employee[] employees = new Employee[10];
     private int departmentId;
+
+    public void addEmployee(Employee employee) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] == null) {
+                employees[i] = employee;
+            }else {
+                System.out.println("you've reached the maximum number of employees");
+            }
+        }
+    }
 }
