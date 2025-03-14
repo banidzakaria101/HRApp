06657edb0e07e2631ce0237254bf9837142d778b
zakariabanid101 <banidzakaria2@gmail.com>
2025-03-14 13:53:01 +0000

    i initialize the project and i added employee class

---
+++ src/Employee.java
@@ -0,0 +1,46 @@
+public class Employee {
+    private String name;
+    private int salary;
+    private String department;
+    private int id;
+
+    public Employee(String name, int salary, String department, int id) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
